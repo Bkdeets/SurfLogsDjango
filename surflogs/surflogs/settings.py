de7d555f388d8910e0a1ebf6284c20fdfc29f29e
Britton <brittonkdeets@gmail.com>
2019-04-18 19:43:15 -0400

    static files

--- surflogs/surflogs/settings.py
+++ surflogs/surflogs/settings.py
@@ -143,10 +143,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
