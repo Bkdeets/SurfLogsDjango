83f7148bb3263844e83d2d96389441c2fd17a719
Britton <brittonkdeets@gmail.com>
2019-04-18 22:26:54 -0400

    revert wsgi

--- surflogs/surflogs/wsgi.py
+++ surflogs/surflogs/wsgi.py
@@ -6,21 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-# import os
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surflogs.surflogs.settings")
-#
-# # This application object is used by the development server
-# # as well as any WSGI server configured to use this file.
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-
-
-
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surflogs.surflogs.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
