59cadc97653a2f6fa3bdf0e0ddfca73247e8f1eb
Britton <brittonkdeets@gmail.com>
2019-04-18 22:14:54 -0400

    wsgi

--- surflogs/surflogs/wsgi.py
+++ surflogs/surflogs/wsgi.py
@@ -6,11 +6,21 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-import os
+# import os
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surflogs.surflogs.settings")
+#
+# # This application object is used by the development server
+# # as well as any WSGI server configured to use this file.
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surflogs.surflogs.settings")
+
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
