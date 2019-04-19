47fe8a06151ab5c5d9f2c7ef03a6a87d786f85f8
Britton <brittonkdeets@gmail.com>
2019-04-18 22:40:56 -0400

    trying

--- surflogs/surflogs/urls.py
+++ surflogs/surflogs/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('', include('logs.urls')),
+    path('', include('surflogs.logs.urls')),
     path('admin/', admin.site.urls)
 ]
