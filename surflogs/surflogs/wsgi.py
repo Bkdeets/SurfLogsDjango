7f9aaba73da776786ad0df28585eeb46bb947573
Britton <brittonkdeets@gmail.com>
2019-04-18 20:59:35 -0400

    gunicorn

--- surflogs/surflogs/wsgi.py
+++ surflogs/surflogs/wsgi.py
@@ -6,13 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surflogs.settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
 from django.core.wsgi import get_wsgi_application
-import django.core.handlers.wsgi
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'surflogs.settings')
-
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
