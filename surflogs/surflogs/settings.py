1c5157a844a15fdac8ee7173e95e17f84aa4cdb8
Britton <brittonkdeets@gmail.com>
2019-04-18 20:10:23 -0400

    middleware

--- surflogs/surflogs/settings.py
+++ surflogs/surflogs/settings.py
@@ -74,11 +74,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-MIDDLEWARE_CLASSES = (
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-)
 
 ROOT_URLCONF = 'surflogs.urls'
 
